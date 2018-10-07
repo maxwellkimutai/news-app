8614780542a156534da3874885990ef79a9b884f
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-10-07 23:15:20 +0300

    add articles class

--- app/models.py
+++ app/models.py
@@ -9,3 +9,16 @@ class Source:
         self.description = description
         self.category = category
         self.language = language
+
+class Article:
+    '''
+    Articles class to define Articles Objects
+    '''
+
+    def __init__(self,author,title,description,url,image,publishedAt):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image = image
+        self.publishedAt = publishedAt
