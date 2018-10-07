fbc9e3f9a506988c41748cd93d70019527f877ac
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-10-07 23:14:53 +0300

    add sources class

---
+++ app/models.py
@@ -0,0 +1,11 @@
+class Source:
+    '''
+    Source class to define Source Objects
+    '''
+
+    def __init__(self,id,name,description,category,language):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.category = category
+        self.language = language
